c9eb0c0f3bf5ec372c984af0a4b49db01c2c7e44
Gyeongrak Choi <peter6617@gmail.com>
2024-05-29 23:20:31 +0900

    타임리프 Test

--- board/src/main/java/com/raknrak/board/controller/SampleController.java
+++ board/src/main/java/com/raknrak/board/controller/SampleController.java
@@ -5,15 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
     
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello")
     public void hello(Model model) {
         log.info("hello....................");
         
         model.addAttribute("msg", "Hello World");
         
+    }
+
+    @GetMapping(value = "/ex/ex1")
+    public void ex1(Model model){
+
+        List<String> list = Arrays.asList("AAA","BBB","CCC","DDD");
+
+        model.addAttribute("list", list);
+
     }
 }
