674aa7a9f0596346e07ef21d6ba3154c6ecdf090
Gyeongrak Choi <peter6617@gmail.com>
2024-05-30 23:19:25 +0900

    SampleDTO 추가

--- board/src/main/java/com/raknrak/board/controller/SampleController.java
+++ board/src/main/java/com/raknrak/board/controller/SampleController.java
@@ -6,26 +6,73 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
 public class SampleController {
-    
+
     @GetMapping(value = "/hello")
     public void hello(Model model) {
         log.info("hello....................");
-        
+
         model.addAttribute("msg", "Hello World");
-        
+
     }
 
     @GetMapping(value = "/ex/ex1")
-    public void ex1(Model model){
+    public void ex1(Model model) {
 
-        List<String> list = Arrays.asList("AAA","BBB","CCC","DDD");
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
 
         model.addAttribute("list", list);
 
     }
+
+    //    동일한 데이터를 다르게 출력해주는 인라인 기능 -> 다양한 종류의 데이터 Model에 담기
+    class SampleDTO {
+        private String p1,p2,p3;
+
+        public String getP1() {
+            return p1;
+        }
+        public String getP2() {
+            return p2;
+        }
+        public String getP3() {
+            return p3;
+        }
+    }
+
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model) {
+
+        log.info("ex/ex2................");
+
+        List<String> strList = IntStream.range(1,10)// 1부터 10 미만
+                .mapToObj(i -> "Data"+i)
+                .collect(Collectors.toList());
+
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A","AAAA");
+        map.put("B","BBBB");
+
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 ="Value -- p1";
+        sampleDTO.p2 ="Value -- p2";
+        sampleDTO.p3 ="Value -- p3";
+
+        model.addAttribute("dto", sampleDTO);
+    }
 }
+
+
