b3be27bd3e9e1f02237e63b1e08fc9ef63bef7dd
Gyeongrak Choi <peter6617@gmail.com>
2024-06-04 23:28:19 +0900

    Insert test

---
+++ board/src/test/java/com/raknrak/board/repository/BoardRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.raknrak.board.repository;
+
+import com.raknrak.board.domain.Board;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void InsertTest(){
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title...." + 1)
+                    .content("content...." + 1)
+                    .writer("user..." + 1)
+                    .build();
+
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getTitle());
+        });
+
+    }
+
+}
+
